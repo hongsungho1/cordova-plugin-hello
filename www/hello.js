5e418b454d24d3ba98b22299c9935df7f0961157
hong sungho <32221345+hongsungho1@users.noreply.github.com>
2022-09-15 00:22:35 +0900

    Update hello.js

--- www/hello.js
+++ www/hello.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    greet: function (name, successCallback, errorCallback, zzz) {
+        cordova.exec(successCallback, errorCallback, zzz, "Hello", "greet", [name]);
     }
 };
